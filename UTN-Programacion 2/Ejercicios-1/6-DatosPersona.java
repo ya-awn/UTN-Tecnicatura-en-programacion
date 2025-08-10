5f05e96da04d107fb566af0dc566c2f07344d43c
ya.awn <mezakenyi@gmail.com>
2025-08-10 17:05:52 -0300

    mas ejercicios

---
+++ UTN-Programacion 2/Ejercicios-1/6-DatosPersona.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author kenyi
+ */
+public class DatosPersona {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Nombre: Juan Perez\nEdad: 30 años\nDireccion: \"Calle Falsa 123\"");
+    }
+    
+}
---
+++ UTN-Programacion 2/Ejercicios-1/AnalisisCodigo.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author kenyi
+ */
+public class AnalisisCodigo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // declara e inicializa una variable
+        // expresion 10 (genera un valor)
+        int x = 10;
+        
+        // agrega un valor a la variable x
+        // "x + 5" (suma que produce un nuevo valor)
+        x = x + 5;
+        
+        // llamada del metodo para imprimir en consola
+        // "x" (valor de la variable que se pasa como argumento)
+        System.out.println(x);
+
+        //Diferencia:
+        // Una expresión es cualquier fragmento de código que produce
+        // un valor (como 10 o x + 5). Una instrucción es una orden completa
+        // que realiza una acción, como declarar variables, asignar valores o
+        // ejecutar métodos; puede contener una o varias expresiones dentro de ella.
+    }
+    
+}
