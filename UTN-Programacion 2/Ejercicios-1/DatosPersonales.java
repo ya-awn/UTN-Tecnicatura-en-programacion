69ca4339929ee8d4db15fb1bff031271b821f613
ya.awn <mezakenyi@gmail.com>
2025-08-10 10:51:31 -0300

    Agregando Ejercicios java

---
+++ UTN-Programacion 2/Ejercicios-1/DatosPersonales.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+/**
+ *
+ * @author kenyi
+ */
+public class DatosPersonales {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // crea el objeto scanner para leer datos desde consola
+        Scanner input = new Scanner(System.in);
+        
+        // solicita nombre
+        System.out.println("Ingresa tu nombre: ");
+        String nombre = input.nextLine();
+        
+        // solicita edad
+        System.out.println("Ingresa tu edad: ");
+        int edad = input.nextInt();
+        
+        // mostrar los datos en pantalla
+        System.out.println("Tu nombre es: " + nombre);
+        System.out.println("Tu edad es: " + edad + " años");
+        
+        // cerrar el scanner
+        input.close();
+    }
+    
+}
---
+++ UTN-Programacion 2/Ejercicios-1/OperacionesBasicas.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+/**
+ *
+ * @author kenyi
+ */
+public class OperacionesBasicas {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        // solicita los numeros
+        System.out.println("Ingresa el primer numero entero: ");
+        int num1 = input.nextInt();
+        
+        System.out.println("ingresa el segundo numero entero: ");
+        int num2 = input.nextInt();
+        
+        // operaciones 
+        int suma = num1 + num2;
+        int resta = num1 - num2;
+        int multiplicacion = num1 * num2;
+        
+        // division con formato inteligente
+        String divisionStr;
+        if (num2 != 0) {
+            if (num1 % num2 == 0){
+                // division exacta - entero
+                divisionStr = String.valueOf(num1 / num2);
+            } else {
+                // division con decimales - double
+                divisionStr = String.valueOf((double) num1 / num2);
+            }
+        } else {
+            divisionStr = "No se puede dividir entre cero";
+        }
+        
+        // mostrar resultados
+        System.out.println("Suma: " + suma);
+        System.out.println("Resta: " + resta);
+        System.out.println("Multiplicacion: " + multiplicacion);
+        System.out.println("Division: " + divisionStr);
+        
+        input.close();
+    }
+    
+}
